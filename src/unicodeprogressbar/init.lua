1d7987bc5b6a1a00102c6d2ce96ed2bf9b82d1ca
khanmenthe <54005405+khanmenthe@users.noreply.github.com>
2024-12-09 18:03:19 +0100

    added comments to main module

--- src/unicodeprogressbar/init.lua
+++ src/unicodeprogressbar/init.lua
@@ -26,25 +26,25 @@ end
 -- Generates a text progress bar 
 function unicodeProgressBar.generateBar(value: number, minRangeValue: number, maxRangeValue: number, length: number, background_block: keyof<typeof(CONSTANTS.BACKGROUND_BLOCKS)>?): string
 	background_block = 	background_block or SETTINGS.DEFAULT_BACKGROUND
-
 	local bar: string = ""
 
-	local normalizedValue: number = (value - minRangeValue) / (maxRangeValue - minRangeValue)
-	local scaledToLengthValue: number = length * normalizedValue
-
-	local scaledToLengthValueWhole: number, scaledToLengthValueMantissa: number = math.modf(scaledToLengthValue)
+	local normalizedValue = (value - minRangeValue) / (maxRangeValue - minRangeValue) -- normilizing formula
+	local scaledToLengthValue = length * normalizedValue
+	local scaledToLengthValueWhole, scaledToLengthValueMantissa = math.modf(scaledToLengthValue) 
 
 	local quantizedScaledValueMantissa: number = quantize(scaledToLengthValueMantissa, CONSTANTS.INTERVAL)
+
 
 	for _ = 1, scaledToLengthValueWhole, 1 do
 		bar ..= CONSTANTS.BLOCKS[8]
 	end
-	bar ..= CONSTANTS.BLOCKS[quantizedScaledValueMantissa * CONSTANTS.INTERVAL_DENOMINATOR]
+	bar ..= CONSTANTS.BLOCKS[quantizedScaledValueMantissa * CONSTANTS.INTERVAL_DENOMINATOR] -- this adds the last char of the bar
 	
-	for _ = 1, length - scaledToLengthValueWhole, 1 do
+	for _ = 1, length - scaledToLengthValueWhole, 1 do -- this adds the background blocks
 		bar ..= CONSTANTS.BACKGROUND_BLOCKS[background_block]
 	end
 	
+
 	return bar
 end
 
